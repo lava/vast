0ad4332b4ef345e9ace38946eee12929a6e8432e
Dominik Lohmann <mail@dominiklohmann.de>
2021-04-15 09:51:17 +0200

    Move PCAP import/export into a plugin
    This (rather large) commit creates two new plugin types, namely
    `reader_plugin` and `writer_plugin`, and creates a PCAP plugin that
    implements both. Additionally, it fixes unit tests for plugins, which
    were unable to actually use the plugin up until now.

--- cmake/VASTRegisterPlugin.cmake
+++ cmake/VASTRegisterPlugin.cmake
@@ -3,7 +3,8 @@ include_guard(GLOBAL)
 function (VASTRegisterPlugin)
   include(GNUInstallDirs)
 
-  cmake_parse_arguments(PLUGIN "" "TARGET" "SOURCES;TEST_SOURCES" ${ARGN})
+  cmake_parse_arguments(PLUGIN "" "TARGET;ENTRYPOINT" "SOURCES;TEST_SOURCES"
+                        ${ARGN})
 
   # A replacement for target_link_libraries that links static libraries using
   # the platform-specific whole-archive options. Please test any changes to this
@@ -29,24 +30,52 @@ function (VASTRegisterPlugin)
       FATAL_ERROR "TARGET must be specified in call to VASTRegisterPlugin")
   endif ()
 
-  if (NOT PLUGIN_SOURCES)
-    message(
-      FATAL_ERROR "SOURCES must be specified in call to VASTRegisterPlugin")
+  if (NOT PLUGIN_ENTRYPOINT)
+    list(LENGTH PLUGIN_SOURCES num_sources)
+    if (num_sources EQUAL 1)
+      list(GET PLUGIN_SOURCES 0 PLUGIN_ENTRYPOINT)
+      set(PLUGIN_SOURCEs "")
+    else ()
+      message(
+        FATAL_ERROR "ENTRYPOINT must be specified in call to VASTRegisterPlugin"
+      )
+    endif ()
   endif ()
 
+  # Create a static library target for our plugin.
+  add_library(${PLUGIN_TARGET} STATIC ${PLUGIN_ENTRYPOINT} ${PLUGIN_SOURCES})
+  target_link_libraries(
+    ${PLUGIN_TARGET}
+    PUBLIC vast::libvast
+    PRIVATE vast::internal)
+
+  # Use static versions of VAST_REGISTER_PLUGIN family of macros.
+  target_compile_definitions(${PLUGIN_TARGET}
+                             PRIVATE VAST_ENABLE_STATIC_PLUGINS_INTERNAL)
+
   if (VAST_ENABLE_STATIC_PLUGINS)
-    # Create a static library target for our plugin.
-    add_library(${PLUGIN_TARGET} STATIC ${PLUGIN_SOURCES})
-
-    # Use static versions of VAST_REGISTER_PLUGIN family of macros.
-    target_compile_definitions(${PLUGIN_TARGET}
-                               PRIVATE VAST_ENABLE_STATIC_PLUGINS_INTERNAL)
-
     # Link our static library against the vast binary directly.
     target_link_whole_archive(vast PRIVATE ${PLUGIN_TARGET})
   else ()
     # Create a shared library target for our plugin.
-    add_library(${PLUGIN_TARGET} SHARED ${PLUGIN_SOURCES})
+    add_library(${PLUGIN_TARGET}-shared SHARED ${PLUGIN_ENTRYPOINT})
+    target_link_libraries(${PLUGIN_TARGET}-shared PRIVATE ${PLUGIN_TARGET}
+                                                          vast::internal)
+
+    # Install the plugin library to <libdir>/vast/plugins, and also configure
+    # the library output directory accordingly.
+    set_target_properties(
+      ${PLUGIN_TARGET}-shared
+      PROPERTIES LIBRARY_OUTPUT_DIRECTORY
+                 "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/vast/plugins"
+                 OUTPUT_NAME "${PLUGIN_TARGET}")
+    install(TARGETS ${PLUGIN_TARGET}-shared
+            DESTINATION "${CMAKE_INSTALL_LIBDIR}/vast/plugins")
+
+    # Ensure the man-page target is generated after all plugins.
+    if (TARGET vast-man)
+      add_dependencies(vast-man ${PLUGIN_TARGET}-shared)
+    endif ()
   endif ()
 
   if (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/schema")
@@ -81,26 +110,6 @@ function (VASTRegisterPlugin)
         add_dependencies(vast-schema vast-schema-${plugin_schema_file_hash})
       endif ()
     endforeach ()
-  endif ()
-
-  # Install the plugin library to <libdir>/vast/plugins, and also configure the
-  # library output directory accordingly.
-  install(TARGETS ${PLUGIN_TARGET}
-          DESTINATION "${CMAKE_INSTALL_LIBDIR}/vast/plugins")
-  set_target_properties(
-    ${PLUGIN_TARGET}
-    PROPERTIES LIBRARY_OUTPUT_DIRECTORY
-               "${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/vast/plugins")
-
-  # Implicitly link plugins against vast::libvast and vast::internal.
-  target_link_libraries(
-    ${PLUGIN_TARGET}
-    PUBLIC vast::libvast
-    PRIVATE vast::internal)
-
-  # Ensure the man-page target is generated after all plugins.
-  if (TARGET vast-man)
-    add_dependencies(vast-man ${PLUGIN_TARGET})
   endif ()
 
   # Setup unit tests.
